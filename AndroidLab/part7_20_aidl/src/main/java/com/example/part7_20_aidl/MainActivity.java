12f65353f255dc1e7180f0d559d091899b5981bb
kkang <kkang104@gmail.com>
2019-06-15 12:02:59 +0900

    Add files via upload

---
+++ AndroidLab/part7_20_aidl/src/main/java/com/example/part7_20_aidl/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.part7_20_aidl;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by kkang
+ * 깡샘의 안드로이드 프로그래밍 - 루비페이퍼
+ * 위의 교재에 담겨져 있는 코드로 설명 및 활용 방법은 교제를 확인해 주세요.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }
+    }
+}
