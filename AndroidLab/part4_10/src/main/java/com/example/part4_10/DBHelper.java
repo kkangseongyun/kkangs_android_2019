92e95829f24f831edd436d6307ed5ae4bab2e39a
kkang <kkang104@gmail.com>
2019-06-15 11:46:56 +0900

    Add files via upload

---
+++ AndroidLab/part4_10/src/main/java/com/example/part4_10/DBHelper.java
@@ -0,0 +1,56 @@
+package com.example.part4_10;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ * Created by kkang
+ * 깡샘의 안드로이드 프로그래밍 - 루비페이퍼
+ * 위의 교제에 담겨져 있는 코드로 설명 및 활용 방법은 교제를 확인해 주세요.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION=8;
+
+    public DBHelper(Context context){
+        super(context, "datadb", null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String tableSql="create table tb_data ("+
+                "_id integer primary key autoincrement," +
+                "name not null," +
+                "content)";
+
+        db.execSQL(tableSql);
+
+        db.execSQL("insert into tb_data (name, content) values ('류현진','화이팅 해라')");
+        db.execSQL("insert into tb_data (name, content) values ('오승환','돌직구 장난 아니구나.')");
+
+
+        String driverTable="create table tb_drive ("+
+                "_id integer primary key autoincrement," +
+                "title," +
+                "date," +
+                "type)";
+        db.execSQL(driverTable);
+        db.execSQL("insert into tb_drive (title, date, type) values ('안드로이드','최종 수정 날짜 : 2월 6일', 'doc')");
+        db.execSQL("insert into tb_drive (title, date, type) values ('db.zip','최종 수정 날짜 : 1월 16일', 'file')");
+        db.execSQL("insert into tb_drive (title, date, type) values ('하이브리드','최종 수정 날짜 : 1월 8일', 'doc')");
+        db.execSQL("insert into tb_drive (title, date, type) values ('이미지1','최종 수정 날짜 : 1월 1일', 'img')");
+        db.execSQL("insert into tb_drive (title, date, type) values ('Part4','최종 수정 날짜 : 12월 24일', 'file')");
+        db.execSQL("insert into tb_drive (title, date, type) values ('Angular','최종 수정 날짜 : 12월 6일', 'doc')");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(newVersion == DATABASE_VERSION){
+            db.execSQL("drop table tb_data");
+            db.execSQL("drop table tb_drive");
+            onCreate(db);
+        }
+    }
+}
---
+++ AndroidLab/part4_10/src/main/java/com/example/part4_10/DriveHolder.java
@@ -0,0 +1,23 @@
+package com.example.part4_10;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+/**
+ * Created by kkang
+ * 깡샘의 안드로이드 프로그래밍 - 루비페이퍼
+ * 위의 교제에 담겨져 있는 코드로 설명 및 활용 방법은 교제를 확인해 주세요.
+ */
+public class DriveHolder {
+    public ImageView typeImageView;
+    public TextView titleView;
+    public TextView dateView;
+    public ImageView menuImageView;
+
+    public DriveHolder(View root){
+        typeImageView=root.findViewById(R.id.custom_item_type_image);
+        titleView=root.findViewById(R.id.custom_item_title);
+        dateView=root.findViewById(R.id.custom_item_date);
+        menuImageView=root.findViewById(R.id.custom_item_menu);
+    }
+}
