18f718912ee87f031ea420b054c4705f34805b1e
kkang <kkang104@gmail.com>
2019-06-15 11:41:03 +0900

    Add files via upload

---
+++ AndroidLab/part2_mission/src/main/java/com/example/part2_mission/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.part2_mission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+/**
+ * Created by kkang
+ * 깡샘의 안드로이드 프로그래밍 - 루비페이퍼
+ * 위의 교제에 담겨져 있는 코드로 설명 및 활용 방법은 교제를 확인해 주세요.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn=findViewById(R.id.messenger_btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast toast=Toast.makeText(this, "ok button click....", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
